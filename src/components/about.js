e7d5da873d0a945402497de2873fb0eaca602ac4
Justin <jtn8001@gmail.com>
2021-11-09 16:30:48 -0500

    v1.0

--- src/components/about.js
+++ src/components/about.js
@@ -1,5 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import sanityClient from "../client";
+import manarola from "../manarola.jpg";
+import ImageUrlBuilder from "@sanity/image-url";
+import BlockContent from "@sanity/block-content-to-react";
+
+const builder = ImageUrlBuilder(sanityClient);
+function urlFor(source) {
+  return builder.image(source);
+}
 
 export default function About() {
-    return <h1>About Page</h1>
+  const [author, setAuthor] = useState(null);
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "author"]{
+            name,
+            bio,
+            "authorImage": image.asset->url
+        }`
+      )
+      .then((data) => setAuthor(data[0]))
+      .catch(console.error);
+  }, []);
+
+  if (!author) return <div>Loading...</div>;
+
+  return (
+    <main className="relative">
+      <img src={manarola} alt="Manarola Italy" className="absolute w-full" />
+      <div className="p-10 lg:pt-48 container mx-auto relative">
+        <section className="bg-dark rounded-lg shadow-2xl lg:flex p-20">
+          <img
+            src={urlFor(author.authorImage).url()}
+            className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8"
+            alt={author.name}
+          />
+          <div className="text-lg flex felx-col justify-center">
+            <h1 className="cursive text-6xl text-white mb-4">
+              Howdy! I'm <span className="text-orange">{author.name}</span>
+            </h1>
+            <div className="prose lg:prose-xl text-white">
+                <BlockContent blocks={author.bio} projectId="dbnnjc3i" dataset="production"/>
+            </div>
+          </div>
+        </section>
+      </div>
+    </main>
+  );
 }
